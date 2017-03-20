a888c09180cac3a6f5c4bb53534f26d37d5495bb
jona8690 <jona8690@edu.eal.dk>
2017-03-20 11:18:48 +0100

    Delete Tests

--- _CODE/FynBusBestOffer/Core.UnitTests/OffersTest.cs
+++ _CODE/FynBusBestOffer/Core.UnitTests/OffersTest.cs
@@ -9,9 +9,18 @@ namespace Core.UnitTests {
 	public class OffersTest {
 		RepositoryOffers RepoOffers;
 
-		static Contractor _TestContractor = new Contractor();
-		static Offer _TestOffer = new Offer(160867, 1, 284, _TestContractor, 0, 0);
+		static Contractor _TestContractor1 = new Contractor();
+		static Contractor _TestContractor2 = new Contractor();
+		static Contractor _TestContractor3 = new Contractor();
 
+		static Offer _TestOffer1 = new Offer(160867, 1, 284, _TestContractor1, 0, 0);
+		static Offer _TestOffer2 = new Offer(163900, 1, 300, _TestContractor1, 0, 0);
+		static Offer _TestOffer3 = new Offer(161170, 10, 123, _TestContractor1, 0, 0);
+		static Offer _TestOffer4 = new Offer(163905, 2500, 456, _TestContractor2, 0, 0);
+		static Offer _TestOffer5 = new Offer(167514, 42, 789, _TestContractor2, 0, 0);
+		static Offer _TestOffer6 = new Offer(169856, 624, 852, _TestContractor2, 0, 0);
+		static Offer _TestOffer7 = new Offer(160456, 777, 479, _TestContractor3, 1, 0);
+		
 		[TestInitialize]
 		public void PrepareTests() {
 			RepoOffers = new RepositoryOffers();
@@ -19,16 +28,39 @@ namespace Core.UnitTests {
 
 		[TestMethod]
 		public void CreateOfferFromObject() {
-			RepoOffers.AddOffer(_TestOffer);
+			RepoOffers.AddOffer(_TestOffer1);
 			List<Offer> OffersList = RepoOffers.GetAllOffers();
-			Assert.IsTrue(OffersList.Contains(_TestOffer));
+			Assert.IsTrue(OffersList.Contains(_TestOffer1));
 		}
 
 		[TestMethod]
 		public void CreateOfferFromText() {
-			RepoOffers.AddOffer(160867, 1, 284, _TestContractor, 0, 0);
+			RepoOffers.AddOffer(160867, 1, 284, _TestContractor1, 0, 0);
 			Offer CompareOffer = RepoOffers.GetOfferByID(160867);
-			Assert.AreEqual(_TestOffer, CompareOffer); // Override the Equals method on Offer Class
+			Assert.AreEqual(_TestOffer1, CompareOffer); // Override the Equals method on Offer Class
+		}
+
+		[TestMethod]
+		public void DeleteOfferByID() {
+			RepoOffers.AddOffer(_TestOffer1);
+			RepoOffers.DeleteOffer(160867);
+			List<Offer> OffersList = RepoOffers.GetAllOffers();
+			Assert.AreEqual(0, OffersList.Count);
+		}
+
+		[TestMethod]
+		public void DeleteOffersByContractor() {
+			RepoOffers.AddOffer(_TestOffer1);
+			RepoOffers.AddOffer(_TestOffer2);
+			RepoOffers.AddOffer(_TestOffer3);
+			RepoOffers.AddOffer(_TestOffer4);
+			RepoOffers.AddOffer(_TestOffer5);
+			RepoOffers.AddOffer(_TestOffer6);
+			RepoOffers.AddOffer(_TestOffer7);
+
+			RepoOffers.DeleteOffer(_TestContractor1);
+			List<Offer> OffersList = RepoOffers.GetAllOffers();
+			Assert.AreEqual(4, OffersList.Count);
 		}
 	}
 }
