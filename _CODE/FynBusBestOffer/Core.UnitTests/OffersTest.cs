e25dab14d65900b6b97cf40cc36e63e2a97b39e1
jona8690 <jona8690@edu.eal.dk>
2017-03-20 10:58:34 +0100

    Added 2 tests

---
+++ _CODE/FynBusBestOffer/Core.UnitTests/OffersTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Core.UnitTests {
+
+	[TestClass]
+	public class OffersTest {
+		RepositoryOffers RepoOffers;
+
+		static Contractor _TestContractor = new Contractor();
+		static Offer _TestOffer = new Offer(160867, 1, 284, _TestContractor, 0, 0);
+
+		[TestInitialize]
+		public void PrepareTests() {
+			RepoOffers = new RepositoryOffers();
+		}
+
+		[TestMethod]
+		public void CreateOfferFromObject() {
+			RepoOffers.AddOffer(_TestOffer);
+			List<Offer> OffersList = RepoOffers.GetAllOffers();
+			Assert.IsTrue(OffersList.Contains(_TestOffer));
+		}
+
+		[TestMethod]
+		public void CreateOfferFromText() {
+			RepoOffers.AddOffer(160867, 1, 284, _TestContractor, 0, 0);
+			Offer CompareOffer = RepoOffers.GetOfferByID(160867);
+			Assert.AreEqual(_TestOffer, CompareOffer); // Override the Equals method on Offer Class
+		}
+	}
+}
