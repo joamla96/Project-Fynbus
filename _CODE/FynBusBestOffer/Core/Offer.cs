7ba2251ce36a522fa1b4b734b698672784fd1b2f
Roxana Ion <ionroxana95@yahoo.com>
2017-03-22 13:43:40 +0100

    changed name

--- _CODE/FynBusBestOffer/Core/Offer.cs
+++ _CODE/FynBusBestOffer/Core/Offer.cs
@@ -8,17 +8,17 @@ namespace Core {
     public class Offer
     {
         private int OfferSeqNr;
-        private int CarNr;
+        private Route Route;
         private int PricePerHour;
         private int RoutePriority;
         private int ContractorPriority;
         private Contractor Contractor;
         public double TotalContractValue { get; set; }
 
-        public Offer(int offerseqnr, int carnr, int priceperhour, Contractor contractor, int routepriority, int contractorpriority)
+        public Offer(int offerseqnr, Route route, int priceperhour, Contractor contractor, int routepriority, int contractorpriority)
         {
             this.OfferSeqNr = offerseqnr;
-            this.CarNr = carnr;
+            this.Route = route;
             this.PricePerHour = priceperhour;
             this.Contractor = contractor;
             this.RoutePriority = routepriority;
