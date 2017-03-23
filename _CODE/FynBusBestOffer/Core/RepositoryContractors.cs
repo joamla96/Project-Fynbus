dfd69adf3a91ca7053b735360e93ae13b90b8c0b
Hedviga <hedv0147@edu.eal.dk>
2017-03-23 14:44:24 +0100

    Contractors are done

--- _CODE/FynBusBestOffer/Core/RepositoryContractors.cs
+++ _CODE/FynBusBestOffer/Core/RepositoryContractors.cs
@@ -5,6 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Core {
-	public class RepositoryContractors {
-	}
+    public class RepositoryContractors
+    {
+        List<Contractor> _contractor = new List<Contractor>();
+        public void AddContractor(Contractor contractor)
+        {
+            _contractor.Add(contractor);
+        }
+
+        public List<Contractor> GetAllContractors()
+        {
+            return _contractor;
+        }
+
+        public void AddContractor(int contractorseqnr, string contractorname, string companyname, string companyemail, int[] cartypearray)
+        {
+            Contractor contractor = new Contractor(contractorseqnr, contractorname, companyname, companyemail, cartypearray);
+            _contractor.Add(contractor);
+        }
+
+        public Contractor GetContractorByID(int contractorseqnr)
+        {
+            Contractor result = null;
+            foreach (Contractor element in _contractor)
+            {
+                if (element.ContractorSeqNr == contractorseqnr)
+                {
+                    result = element;
+                }
+            }
+            return result;
+        }
+
+        public void DeleteContractor(int contractorseqnr)
+        {
+            Contractor toDelete = this.GetContractorByID(contractorseqnr);
+            _contractor.Remove(toDelete);
+        }
+    }
 }
