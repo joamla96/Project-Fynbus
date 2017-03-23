2d7ad48959ca6cbc88329277463d727beb6a1531
Jonas Laursen <jona8690@edu.eal.dk>
2017-03-23 11:48:06 +0100

    Split test

--- _CODE/FynBusBestOffer/Core.IntegrationTests/GetBestOffer.cs
+++ _CODE/FynBusBestOffer/Core.IntegrationTests/GetBestOffer.cs
@@ -1,72 +1,79 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Core;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
 
-//namespace Core.IntegrationTests
-//{
-//	[TestClass]
-//	public class GetBestOffer
-//	{
-//		RepositoryOffers _repoOffers;
-//		RepositoryRoutes _repoRoutes;
-//		RepositoryContractors _repoContractors;
+namespace Core.IntegrationTests
+{
+	[TestClass]
+	public class GetBestOffer
+	{
+		RepositoryOffers _repoOffers;
+		RepositoryRoutes _repoRoutes;
+		RepositoryContractors _repoContractors;
 
-//		// SeqNr, Name, Company, Email, CarType 2, 3, 5, 6, 7
-//		static Contractor _testContractor1 = new Contractor(163460, "Navn A", "Firma A", "Apple@Email.com", new int[] { 3, 0, 0, 0, 0 });
-//		static Contractor _testContractor2 = new Contractor(163478, "Navn B", "Firma B", "Banana@Email.com", new int[] { 2, 0, 0, 0, 0 });
-//		static Contractor _testContractor3 = new Contractor(164758, "Navn C", "Firma C", "Canada@Email.com", new int[] { 1, 0, 0, 0, 0 });
+		// SeqNr, Name, Company, Email, CarType 2, 3, 5, 6, 7
+		static Contractor _testContractor1 = new Contractor(163460, "Navn A", "Firma A", "Apple@Email.com", new int[] { 3, 0, 0, 0, 0 });
+		static Contractor _testContractor2 = new Contractor(163478, "Navn B", "Firma B", "Banana@Email.com", new int[] { 2, 0, 0, 0, 0 });
+		static Contractor _testContractor3 = new Contractor(164758, "Navn C", "Firma C", "Canada@Email.com", new int[] { 1, 0, 0, 0, 0 });
 
-//		// GarantiVognNr, Start, Type, Hours: Normal, Weekend, Holidays
-//		static Route _testRoute1 = new Route(2502, "OUH", 2, 9.5, 10.5, 9.5, 10.5, 9.5, 10.5);
-//		static Route _testRoute2 = new Route(2503, "OUH", 2, 9.5, 10.5, 0, 0, 0, 0);
-//		static Route _testRoute3 = new Route(2504, "OUH", 2, 9.5, 10.5, 0, 0, 9.5, 10.5);
-//		static Route _testRoute4 = new Route(2505, "OUH", 2, 9.5, 10.5, 0, 0, 0, 0);
-//		static Route _testRoute5 = new Route(2506, "Ørbækvej", 2, 9.5, 10.5, 0, 0, 0, 0);
+		// GarantiVognNr, Start, Type, Hours: Normal, Weekend, Holidays
+		static Route _testRoute1 = new Route(2502, "OUH", 2, 9.5, 10.5, 9.5, 10.5, 9.5, 10.5);
+		static Route _testRoute2 = new Route(2503, "OUH", 2, 9.5, 10.5, 0, 0, 0, 0);
+		static Route _testRoute3 = new Route(2504, "OUH", 2, 9.5, 10.5, 0, 0, 9.5, 10.5);
+		static Route _testRoute4 = new Route(2505, "OUH", 2, 9.5, 10.5, 0, 0, 0, 0);
+		static Route _testRoute5 = new Route(2506, "Ørbækvej", 2, 9.5, 10.5, 0, 0, 0, 0);
 
-//		// SeqNr, Route, Price/Hr, Contractor, RoutePrio, ContractorPrio
-//		static Offer _testOffer1 = new Offer(160867, _testRoute1, 284, _testContractor3, 0, 0);
-//		static Offer _testOffer2 = new Offer(163900, _testRoute1, 300, _testContractor2, 0, 0);
+		// SeqNr, Route, Price/Hr, Contractor, RoutePrio, ContractorPrio
+		static Offer _testOffer1 = new Offer(160867, _testRoute1, 284, _testContractor3, 0, 0);
+		static Offer _testOffer2 = new Offer(163900, _testRoute1, 300, _testContractor2, 0, 0);
 
-//		static Offer _testOffer3 = new Offer(161170, _testRoute2, 123, _testContractor3, 0, 0);
-//		static Offer _testOffer4 = new Offer(163905, _testRoute2, 456, _testContractor2, 0, 0);
-//		static Offer _testOffer5 = new Offer(167514, _testRoute2, 789, _testContractor1, 0, 0);
+		static Offer _testOffer3 = new Offer(161170, _testRoute2, 123, _testContractor3, 0, 0);
+		static Offer _testOffer4 = new Offer(163905, _testRoute2, 456, _testContractor2, 0, 0);
+		static Offer _testOffer5 = new Offer(167514, _testRoute2, 789, _testContractor1, 0, 0);
 
-//		static Offer _testOffer6 = new Offer(169856, _testRoute3, 852, _testContractor1, 0, 0);
-//		static Offer _testOffer7 = new Offer(160456, _testRoute3, 479, _testContractor2, 1, 0);
+		static Offer _testOffer6 = new Offer(169856, _testRoute3, 852, _testContractor1, 0, 0);
+		static Offer _testOffer7 = new Offer(160456, _testRoute3, 479, _testContractor2, 1, 0);
 
-//		[TestInitialize]
-//		public void PrepareTests() {
-//			_repoOffers = RepositoryOffers.Instance; // Singletons
-//			_repoOffers.Clear();
+		[TestInitialize]
+		public void PrepareTests() {
+			_repoOffers = RepositoryOffers.Instance; // Singletons
+			_repoOffers.Clear();
 
-//			_repoRoutes = RepositoryRoutes.Instance;
-//			_repoRoutes.Clear();
+			_repoRoutes = RepositoryRoutes.Instance;
+			_repoRoutes.Clear();
 
-//			_repoContractors = RepositoryContractors.Instance;
-//			_repoContractors.Clear();
+			_repoContractors = RepositoryContractors.Instance;
+			_repoContractors.Clear();
 
-//			_repoOffers.Add(_testOffer1);
-//			_repoOffers.Add(_testOffer2);
-//			_repoOffers.Add(_testOffer3);
-//			_repoOffers.Add(_testOffer4);
-//			_repoOffers.Add(_testOffer5);
-//			_repoOffers.Add(_testOffer6);
-//			_repoOffers.Add(_testOffer7);
+			_repoOffers.Add(_testOffer1);
+			_repoOffers.Add(_testOffer2);
+			_repoOffers.Add(_testOffer3);
+			_repoOffers.Add(_testOffer4);
+			_repoOffers.Add(_testOffer5);
+			_repoOffers.Add(_testOffer6);
+			_repoOffers.Add(_testOffer7);
 
-//			_repoRoutes.Add(_testRoute1);
-//			_repoRoutes.Add(_testRoute2);
-//			_repoRoutes.Add(_testRoute3);
-//			_repoRoutes.Add(_testRoute4);
-//			_repoRoutes.Add(_testRoute5);
+			_repoRoutes.Add(_testRoute1);
+			_repoRoutes.Add(_testRoute2);
+			_repoRoutes.Add(_testRoute3);
+			_repoRoutes.Add(_testRoute4);
+			_repoRoutes.Add(_testRoute5);
 
-//			_repoContractors.Add(_testContractor1);
-//			_repoContractors.Add(_testContractor2);
-//			_repoContractors.Add(_testContractor3);
-//		}
+			_repoContractors.Add(_testContractor1);
+			_repoContractors.Add(_testContractor2);
+			_repoContractors.Add(_testContractor3);
+		}
 
-//		[TestMethod]
-//		public void GetBestOfferByRoutes() {
-//			throw new NotImplementedException();
-//		}
-//	}
-//}
+		[TestMethod]
+		public void GetBestOfferByRoutes1() {
+			Offer BestOffer = _testRoute1.GetBestOffer();
+			Assert.AreEqual(BestOffer, _testOffer1);
+		}
+
+		[TestMethod]
+		public void GetBestOfferByRoutes2() {
+			Offer BestOffer = _testRoute2.GetBestOffer();
+			Assert.AreEqual(BestOffer, _testOffer3);
+		}
+	}
+}
