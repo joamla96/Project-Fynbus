73fb75a2607b12ea4bdd6501b981ddd7cb7b5bc9
Jonas Laursen <jona8690@edu.eal.dk>
2017-03-30 13:35:00 +0200

    Moved dealing with cars from route to contractor

--- _CODE/FynBusBestOffer/Core/Contractor.cs
+++ _CODE/FynBusBestOffer/Core/Contractor.cs
@@ -4,39 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core {
-    public class Contractor
-    {
-        public int ContractorSeqNr;
-        public string ContractorName;
-        public string CompanyName;
-        public string CompanyEmail;
-        public int[] CarTypeArray;
+namespace Core
+{
+	public class Contractor
+	{
+		public int ContractorSeqNr;
+		public string ContractorName;
+		public string CompanyName;
+		public string CompanyEmail;
+		public int[] CarTypeArray;
 
-        public int[] CarTypeWonArray;
+		public int[] CarTypeWonArray;
 
-        public Contractor(int contractorseqnr, string contractorname, string companyname, string companyemail, int[] cartypearray)
-        {
-            this.ContractorSeqNr = contractorseqnr;
-            this.ContractorName = contractorname;
-            this.CompanyName = companyname;
-            this.CompanyEmail = companyemail;
-            this.CarTypeArray = cartypearray;
-            this.CarTypeWonArray = new int[] { 0, 0, 0, 0, 0 };
-        }
+		public Contractor(int contractorseqnr, string contractorname, string companyname, string companyemail, int[] cartypearray) {
+			this.ContractorSeqNr = contractorseqnr;
+			this.ContractorName = contractorname;
+			this.CompanyName = companyname;
+			this.CompanyEmail = companyemail;
+			this.CarTypeArray = cartypearray;
+			this.CarTypeWonArray = new int[] { 0, 0, 0, 0, 0 };
+		}
 
-        public Contractor()
-        {
-        }
-        public override bool Equals(object obj)
-        {
-            bool result = false;
-            Contractor contractor = (Contractor)obj;
-            if (this.ContractorSeqNr == contractor.ContractorSeqNr)
-            {
-                result = true;
-            }
-            return result;
-        }
-    }
+		public Contractor() {
+		}
+		public override bool Equals(object obj) {
+			bool result = false;
+			Contractor contractor = (Contractor)obj;
+			if (this.ContractorSeqNr == contractor.ContractorSeqNr) {
+				result = true;
+			}
+			return result;
+		}
+
+		public int GetCarsWonOfType(int type) {
+			int index = this.GetCarTypeForArray(type);
+			int wonCars = this.CarTypeWonArray[index];
+
+			return wonCars;
+		}
+
+		public int GetCarsMaxOfType(int type) {
+			int index = this.GetCarTypeForArray(type);
+			int maxCars = this.CarTypeArray[index];
+
+			return maxCars;
+		}
+
+		public void WonCarOfType(int type) {
+			int index = GetCarTypeForArray(type);
+			this.CarTypeWonArray[index]++;
+		}
+
+		public int GetCarTypeForArray(int cartype) {
+			int index;
+			switch (cartype) {
+				case 2: index = 0; break;
+				case 3:	index = 1; break;
+				case 5:	index = 2; break;
+				case 6: index = 3; break;
+				case 7:	index = 4; break;
+
+				default: throw new IndexOutOfRangeException();
+			}
+			return index;
+		}
+	}
 }
--- _CODE/FynBusBestOffer/Core/Route.cs
+++ _CODE/FynBusBestOffer/Core/Route.cs
@@ -4,109 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core {
-    public class Route
-    {
-        public int CarNr;
-        public string HomeBase;
-        public int CarType;
-        public double WarrantyWeekdaysHours;
-        public double AvailabilityWeekdaysHours;
-        public double WarrantyWeekendHours;
-        public double AvailabilityWeekendHours;
-        public double WarrantyHolidayHours;
-        public double AvailabilityHolidayHours;
+namespace Core
+{
+	public class Route
+	{
+		public int CarNr;
+		public string HomeBase;
+		public int CarType;
+		public double WarrantyWeekdaysHours;
+		public double AvailabilityWeekdaysHours;
+		public double WarrantyWeekendHours;
+		public double AvailabilityWeekendHours;
+		public double WarrantyHolidayHours;
+		public double AvailabilityHolidayHours;
 
-        public Offer OfferWon { get; set; }
+		public Offer OfferWon { get; set; }
 
-        public Route(int carnr, string homebase, int cartype, double warrantyweekdayshours, double availabilityweekdayshours, double warrantyweekendhours, double availabilityweekendhours, double warrantyholidayhours, double availabilityholidayhours)
-        {
-            this.CarNr = carnr;
-            this.HomeBase = homebase;
-            this.CarType = cartype;
-            this.WarrantyWeekdaysHours = warrantyweekdayshours;
-            this.AvailabilityWeekdaysHours = availabilityweekdayshours;
-            this.WarrantyWeekendHours = warrantyweekendhours;
-            this.AvailabilityWeekendHours = availabilityweekendhours;
-            this.WarrantyHolidayHours = warrantyholidayhours;
-            this.AvailabilityHolidayHours = availabilityholidayhours;
-        }
+		public Route(int carnr, string homebase, int cartype, double warrantyweekdayshours, double availabilityweekdayshours, double warrantyweekendhours, double availabilityweekendhours, double warrantyholidayhours, double availabilityholidayhours) {
+			this.CarNr = carnr;
+			this.HomeBase = homebase;
+			this.CarType = cartype;
+			this.WarrantyWeekdaysHours = warrantyweekdayshours;
+			this.AvailabilityWeekdaysHours = availabilityweekdayshours;
+			this.WarrantyWeekendHours = warrantyweekendhours;
+			this.AvailabilityWeekendHours = availabilityweekendhours;
+			this.WarrantyHolidayHours = warrantyholidayhours;
+			this.AvailabilityHolidayHours = availabilityholidayhours;
+		}
 
-        public override bool Equals(object obj)
-        {
-            bool result = false;
-            Route route = (Route)obj;
-            if (this.CarNr == route.CarNr)
-            {
-                result = true;
-            }
-            return result;
-        }
+		public override bool Equals(object obj) {
+			bool result = false;
+			Route route = (Route)obj;
+			if (this.CarNr == route.CarNr) {
+				result = true;
+			}
+			return result;
+		}
 
-        public Offer GetBestOffer()
-        {
-            RepositoryOffers offers = RepositoryOffers.Instance;
-            List<Offer> listOfOffersForRoute = offers.GetOffers(this);
-            int index = 0;
-            listOfOffersForRoute.Sort();
-            Offer bestOffer = listOfOffersForRoute[index];
-            
+		public Offer GetBestOffer() {
+			RepositoryOffers offers = RepositoryOffers.Instance;
+			List<Offer> listOfOffersForRoute = offers.GetOffers(this);
+			int index = 0;
+			listOfOffersForRoute.Sort();
+			Offer bestOffer = listOfOffersForRoute[index];
 
-            bool foundBestOffer = false;
-            do
-            {
-                int arrIndex = GetCarTypeForArray(this.CarType);
-                int startCarAmount = bestOffer.Contractor.CarTypeArray[arrIndex];
-                int wonCarAmount = bestOffer.Contractor.CarTypeWonArray[arrIndex];
 
-                if (startCarAmount < wonCarAmount)
-                {
-                    throw new Exception("Code is BROKEEEEEEEEEEEEEEEEEN!!!! Help! HELP! HEEELP!!!!!");
-                }
-                else if (wonCarAmount < startCarAmount)
-                {
-                    foundBestOffer = true;
-                    bestOffer.Contractor.CarTypeWonArray[arrIndex]++;
-                }
-                else if (startCarAmount == wonCarAmount)
-                {
-                    bestOffer = listOfOffersForRoute[index + 1];
-                }
-            }
-            while (foundBestOffer == false);
+			bool foundBestOffer = false;
+			do {
+				int startCarAmount = bestOffer.Contractor.GetCarsMaxOfType(this.CarType);
+				int wonCarAmount = bestOffer.Contractor.GetCarsWonOfType(this.CarType);
 
-            this.OfferWon = bestOffer;
+				if (startCarAmount < wonCarAmount) {
+					// This... should NEVER EVER EVER happen... j.i.c.
+					throw new Exception("Exception: A contractor has won more cars than they can offer.");
+				} else if (wonCarAmount < startCarAmount) {
+					foundBestOffer = true;
+					bestOffer.Contractor.WonCarOfType(this.CarType);
+				} else if (startCarAmount == wonCarAmount) {
+					bestOffer = listOfOffersForRoute[index + 1];
+				}
+			} while (foundBestOffer == false);
 
-            return bestOffer;
-               
+			this.OfferWon = bestOffer;
 
-        }
-
-        public int GetCarTypeForArray(int cartype)
-        {
-            int index;
-            switch (cartype)
-            {
-                case 2:
-                    index = 0;
-                    break;
-                case 3:
-                    index = 1;
-                    break;
-                case 5:
-                    index = 2;
-                    break;
-                case 6:
-                    index = 3;
-                    break;
-                case 7:
-                    index = 4;
-                    break;
-                default:
-                    throw new IndexOutOfRangeException();
-                        
-            }
-            return index;
-        }
-    }
+			return bestOffer;
+		}
+	}
 }
